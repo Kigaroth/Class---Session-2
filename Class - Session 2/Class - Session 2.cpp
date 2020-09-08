bd63c52f5ef5eb258a7609ed13abc73a087677ec
Kigaroth <hp_20@hotmail.com>
2020-09-08 09:58:30 +0200

    Add project files.

---
+++ Class - Session 2/Class - Session 2.cpp
@@ -0,0 +1,33 @@
+// Class - Session 2.cpp //
+
+#include <iostream>
+#include <string>
+
+//Constants for calculation//
+int days_per_month = 29;
+int hours_per_day = 8;
+int money_per_hour = 1000; //Money in NOK//
+int ceo_manager_bonus = 2000; //Money in NOK//
+
+//String inputs//
+
+std::string full_name = "";
+std::string id_number = "";
+
+//Position//
+int position_answer;
+
+int main()
+{
+    //name input//
+    std::cout << " Please enter your name: ";
+    std::getline(std::cin, full_name);
+
+    //id input//
+    std::cout << " Please enter your ID number: ";
+    std::getline(std::cin, id_number);
+
+    //position input//
+    std::cout << " Which position do you have? Answer by typing 1-3: \n\t 1. "
+}
+
